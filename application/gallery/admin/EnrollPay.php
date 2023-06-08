e8d36d1c9aa1d5c194ca47a8d44c59e043c73f22
火线兔 <silverhawk@foxmail.com>
2023-06-08 13:37:53 +0800

    TuuzMac

--- application/gallery/admin/EnrollPay.php
+++ application/gallery/admin/EnrollPay.php
@@ -63,7 +63,7 @@ class EnrollPay extends Admin
             ->addColumn('date', '创建时间')
             ->addColumn('right_button', '操作', 'btn')
             ->addRightButton('edit') // 添加编辑按钮
-            ->addRightButton('delete') //添加删除按钮
+//            ->addRightButton('delete') //添加删除按钮
             ->setRowList($data_list) // 设置表格数据
             ->setPages($page)
             ->fetch();
