df6cfed5dad5b3b50ebb9df39c677a3aeb9551b9
火线兔 <silverhawk@foxmail.com>
2023-06-08 12:36:20 +0800

    TuuzMac

--- application/gallery/admin/Enroll.php
+++ application/gallery/admin/Enroll.php
@@ -121,7 +121,7 @@ class Enroll extends Admin
             ->setPageTitle('新增') // 设置页面标题
             ->addFormItems([ // 批量添加表单项
                 ['text', 'source', '数据来源', ''],
-                ['text', 'uid', '用户id', "",UserModel::column("id,name")],
+                ['select', 'uid', '用户id', "",UserModel::column("id,name")],
                 ['text', 'tag_id', '报名类型', '', TagModel::column("id,name")],
                 ['text', 'age', '年龄', ''],
                 ['select', 'tag_group_id', '年级组id', '', TagGroupModel::column("id,name")],
