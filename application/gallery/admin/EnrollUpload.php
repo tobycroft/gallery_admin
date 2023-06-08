683c861685c237ab2922a3b7b10228aadf4aee76
火线兔 <silverhawk@foxmail.com>
2023-06-08 15:48:27 +0800

    TuuzMac

--- application/gallery/admin/EnrollUpload.php
+++ application/gallery/admin/EnrollUpload.php
@@ -183,7 +183,7 @@ class EnrollUpload extends Admin
             ->setPageTitle('编辑') // 设置页面标题
             ->addFormItems([ // 批量添加表单项
                 ['hidden', 'id'],
-                ['text', 'uid', '用户手机', '', UserModel::column('id,phone')],
+                ['select', 'uid', '用户手机', '', UserModel::column('id,phone')],
                 ['text', 'enroll_id', '报名人', '', EnrollModel::column('id,name')],
                 ['text', 'title', '标题', ''],
                 ['textarea', 'content', '内容', ''],
