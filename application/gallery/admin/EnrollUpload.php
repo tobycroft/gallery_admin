f62ab8e25c0874cefdf55ad929dcc290b3b46ae1
火线兔 <silverhawk@foxmail.com>
2023-06-08 13:41:32 +0800

    TuuzMac

--- application/gallery/admin/EnrollUpload.php
+++ application/gallery/admin/EnrollUpload.php
@@ -43,8 +43,7 @@ class EnrollUpload extends Admin
 
         return ZBuilder::make('table')
             ->setPageTips("总数量：" . $num2 . "    今日数量：" . $num1, 'danger')
-//            ->setPageTips("总数量：" . $num2, 'danger')
-//            ->setSearchArea([['select', 'school_id', '学校id', "", "", $school], ['text', 'year', '入学年份'], ['text', 'grade', '年级'], ['text', 'class', '班级'],])
+            ->setPageTips("总数量：" . $num2, 'danger')
             ->addTopButton("add")
             ->setPageTitle('列表')
             ->setSearch(['a.id' => 'ID', 'a.uid' => "uid", 'a.name' => '用户名']) // 设置搜索参数
