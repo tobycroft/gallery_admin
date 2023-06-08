3fb1338ee55b25f0470518b1fbabcc165ad1ea08
火线兔 <silverhawk@foxmail.com>
2023-06-08 11:40:02 +0800

    TuuzMac

--- application/gallery/info.php
+++ application/gallery/info.php
@@ -14,19 +14,19 @@
  */
 return [
     // 模块名[必填]
-    'name'        => 'gallery',
+    'name' => 'gallery',
     // 模块标题[必填]
-    'title'       => 'gallery',
+    'title' => 'gallery',
     // 模块唯一标识[必填]，格式：模块名.开发者标识.module
-    'identifier'  => 'gallery.gallery.module',
+    'identifier' => 'gallery.gallery.module',
     // 模块图标[选填]
-    'icon'        => 'fa fa-fw fa-newspaper-o',
+    'icon' => 'fa fa-fw fa-newspaper-o',
     // 模块描述[选填]
     'description' => 'gallery',
     // 开发者[必填]
-    'author'      => 'YourFather',
+    'author' => 'YourFather',
     // 版本[必填],格式采用三段式：主版本号.次版本号.修订版本号
-    'version'     => '1.0.0',
+    'version' => '1.0.0',
     // 模块依赖[可选]，格式[[模块名, 模块唯一标识, 依赖版本, 对比方式]]
     'need_module' => [
     ],
