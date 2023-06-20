a609fb79debd585729e502cdc620d875b1e63d28
火线兔 <silverhawk@foxmail.com>
2023-06-20 15:27:11 +0800

    TuuzMac

--- application/gallery/admin/EnrollPay.php
+++ application/gallery/admin/EnrollPay.php
@@ -74,6 +74,7 @@ class EnrollPay extends Admin
             ])
             ->addTopButton("add")
             ->addTopButton('export')
+            ->addTopButton('enable')
             ->setPageTitle('列表')
             ->setSearch(['name' => '学生姓名', 'phone' => "手机号", "school_name" => "绑定单位", "school_name_show" => "学校"]) // 设置搜索参数
 //            ->addOrder('id,callsign,year,class')
