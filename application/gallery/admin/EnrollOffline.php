966204aa5ccdfe3228e09188a90de5048897d297
火线兔 <silverhawk@foxmail.com>
2023-06-20 16:48:23 +0800

    TuuzMac

--- application/gallery/admin/EnrollOffline.php
+++ application/gallery/admin/EnrollOffline.php
@@ -30,7 +30,7 @@ class EnrollOffline extends Admin
         // 设置表头信息（对应字段名,宽度，显示表头名称）
         $Aoss = new Excel(config('upload_prefix'));
         $ret = $Aoss->create_excel_fileurl($data);
-        url($ret->file_url());
+        $this->success('成功', $ret->file_url(), '_blank');
     }
 
     /**
