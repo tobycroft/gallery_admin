dae205353fb3ceacf5dc4d131e2400e77218f04d
火线兔 <silverhawk@foxmail.com>
2023-06-20 14:57:57 +0800

    TuuzMac

--- application/gallery/admin/EnrollPay.php
+++ application/gallery/admin/EnrollPay.php
@@ -58,15 +58,6 @@ class EnrollPay extends Admin
         $school = EnrollModel::column("id,name");
         $tag = TagModel::column("id,name");
 
-        // 授权按钮
-        $btn_access = [
-            'title' => '导出数据',
-            'icon' => 'fa fa-fw fa-key',
-            'href' => url('export', ["id" => "__id__"]),
-            'target-form' => 'ids',
-            'href' => $this->getDefaultUrl($type, ['_t' => $table_token])
-        ];
-
 
         return ZBuilder::make('table')
             ->setPageTips("总数量：" . $num2 . "    今日数量：" . $num1, 'danger')
@@ -78,7 +69,7 @@ class EnrollPay extends Admin
                 ['text', 'school_name_show', '报名学校',],
             ])
             ->addTopButton("add")
-            ->addTopButton('custom', $btn_access)
+            ->addTopButton('export')
             ->setPageTitle('列表')
             ->setSearch(['name' => '学生姓名', 'phone' => "手机号", "school_name" => "绑定单位", "school_name_show" => "学校"]) // 设置搜索参数
 //            ->addOrder('id,callsign,year,class')
