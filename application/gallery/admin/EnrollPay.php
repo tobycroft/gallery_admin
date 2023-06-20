c6e787962a4c50b0380c2384510176cde8eb2393
火线兔 <silverhawk@foxmail.com>
2023-06-20 16:47:52 +0800

    TuuzMac

--- application/gallery/admin/EnrollPay.php
+++ application/gallery/admin/EnrollPay.php
@@ -49,7 +49,7 @@ class EnrollPay extends Admin
         // 设置表头信息（对应字段名,宽度，显示表头名称）
         $Aoss = new Excel(config('upload_prefix'));
         $ret = $Aoss->create_excel_fileurl($data);
-        $this->success("成功", url($ret->file_url()), "_blank");
+        $this->success("成功", $ret->file_url(), "_blank");
     }
 
     /**
