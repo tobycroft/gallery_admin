dc531793f0e1c88592014253edb5bf8e480663f5
火线兔 <silverhawk@foxmail.com>
2023-06-20 15:31:57 +0800

    TuuzMac

--- application/common/builder/table/Builder.php
+++ application/common/builder/table/Builder.php
@@ -996,6 +996,18 @@ class Builder extends ZBuilder
                 ];
                 break;
 
+
+            // 禁用按钮
+            case 'export':
+                // 默认属性
+                $btn_attribute = [
+                    'title' => '导出',
+                    'icon' => 'fa fa-ban',
+                    'class' => 'btn btn-' . $btn_style['size'] . ' btn-' . $btn_style['style'] . ' ajax-get confirm',
+                    'href' => $this->getDefaultUrl($type, ['ids' => '__id__', '_t' => $table_token, 'field' => $field])
+                ];
+                break;
+
             // 自定义按钮
             default:
                 // 默认属性
