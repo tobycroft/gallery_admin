e2af0ebd57e5f68488929ea3649fd9d3d7717ecf
火线兔 <silverhawk@foxmail.com>
2023-06-26 17:39:24 +0800

    TuuzMac

--- extend/shds/Listdata.php
+++ extend/shds/Listdata.php
@@ -3,6 +3,7 @@
 namespace shds;
 
 use shds\Response\GetActivityList;
+use shds\Response\GetMajor;
 use think\Exception;
 
 class Listdata extends Login
@@ -14,7 +15,7 @@ class Listdata extends Login
 
     public function ActivityId(): int
     {
-        $path = 'megagame/user/userWorks/getActivityList';
+        $path = '/megagame/user/userWorks/getActivityList';
         $ret = \Net::PostJson(config('shds_remote_url') . $path, [], self::$jayParsedAry, $this->header);
         $resp = new GetActivityList($ret);
         if ($resp->isSuccess()) {
@@ -27,4 +28,37 @@ class Listdata extends Login
             throw new Exception($resp->getError());
         }
     }
+
+    public function Major($name)
+    {
+        $path = '/megagame/api/swMajor/getCurrentActivityMajor';
+        $ret = \Net::PostJson(config('shds_remote_url') . $path, [], self::$jayParsedAry, $this->header);
+        $resp = new GetMajor($ret);
+        if ($resp->isSuccess()) {
+            if (count($resp->getRecords()) > 1) {
+                return $resp->getMap()[config('shds_remote_activity')];
+            } else {
+                return $resp->getMap()[array_key_first($resp->getMap())];
+            }
+        } else {
+            throw new Exception($resp->getError());
+        }
+    }
+
+    public function Group($major_name, $group_name)
+    {
+        $path = '/megagame/api/swMajor/getCurrentActivityMajor';
+        $ret = \Net::PostJson(config('shds_remote_url') . $path, [], self::$jayParsedAry, $this->header);
+        $resp = new GetMajor($ret);
+        if ($resp->isSuccess()) {
+            if (count($resp->getRecords()) > 1) {
+                return $resp->getMap()[config('shds_remote_activity')];
+            } else {
+                return $resp->getMap()[array_key_first($resp->getMap())];
+            }
+        } else {
+            throw new Exception($resp->getError());
+        }
+    }
+
 }
--- extend/shds/Response/GetMajor.php
+++ extend/shds/Response/GetMajor.php
@@ -8,7 +8,8 @@ class GetMajor extends Resp
     protected int $code = 0;
 
     protected array $records = [];
-    protected array $map = [];
+    protected array $major_name = [];
+    protected array $major_group = [];
 
 
     public function __construct($json)
@@ -26,7 +27,10 @@ class GetMajor extends Resp
 
         $this->records = $this->data['data']['records'];
         foreach ($this->records as $record) {
-            $map[$record['name']] = $record['activityId'];
+            $this->major_name[$record['majorName']] = $record['majorId'];
+            foreach ($record["childGroup"] as $item) {
+                $this->major_group[$record['majorName']][$item["groupName"]] = $item["groupId"];
+            }
         }
 
     }
@@ -49,3 +53,150 @@ class GetMajor extends Resp
 
 
 }
+
+
+/*
+ * {
+    'code': 'ok',
+    'message': null,
+    'japaneseMessage': null,
+    'data': {
+        'code': 20000,
+        'message': '操作成功',
+        'data': [
+            {
+                'majorId': 16,
+                'fileTypeList': [
+                    '1'
+                ],
+                'majorName': '绘画',
+                'typeId': 0,
+                'childGroup': [
+                    {
+                        'groupName': '小学低年级组',
+                        'groupId': 12
+                    },
+                    {
+                        'groupName': '小学高年级组',
+                        'groupId': 13
+                    },
+                    {
+                        'groupName': '初中组',
+                        'groupId': 14
+                    },
+                    {
+                        'groupName': '高中组',
+                        'groupId': 15
+                    }
+                ]
+            },
+            {
+                'majorId': 17,
+                'fileTypeList': [
+                    '1'
+                ],
+                'majorName': '书法',
+                'typeId': 0,
+                'childGroup': [
+                    {
+                        'groupName': '小学低年级组',
+                        'groupId': 12
+                    },
+                    {
+                        'groupName': '小学高年级组',
+                        'groupId': 13
+                    },
+                    {
+                        'groupName': '初中组',
+                        'groupId': 14
+                    },
+                    {
+                        'groupName': '高中组',
+                        'groupId': 15
+                    }
+                ]
+            },
+            {
+                'majorId': 18,
+                'fileTypeList': [
+                    '1'
+                ],
+                'majorName': '摄影',
+                'typeId': 1,
+                'childGroup': [
+                    {
+                        'groupName': '小学低年级组',
+                        'groupId': 12
+                    },
+                    {
+                        'groupName': '小学高年级组',
+                        'groupId': 13
+                    },
+                    {
+                        'groupName': '初中组',
+                        'groupId': 14
+                    },
+                    {
+                        'groupName': '高中组',
+                        'groupId': 15
+                    }
+                ]
+            },
+            {
+                'majorId': 19,
+                'fileTypeList': [
+                    '1'
+                ],
+                'majorName': '数字美术',
+                'typeId': 1,
+                'childGroup': [
+                    {
+                        'groupName': '小学低年级组',
+                        'groupId': 12
+                    },
+                    {
+                        'groupName': '小学高年级组',
+                        'groupId': 13
+                    },
+                    {
+                        'groupName': '初中组',
+                        'groupId': 14
+                    },
+                    {
+                        'groupName': '高中组',
+                        'groupId': 15
+                    }
+                ]
+            },
+            {
+                'majorId': 30,
+                'fileTypeList': [
+                    '1'
+                ],
+                'majorName': '陶艺',
+                'typeId': 0,
+                'childGroup': [
+                    {
+                        'groupName': '小学低年级组',
+                        'groupId': 12
+                    },
+                    {
+                        'groupName': '小学高年级组',
+                        'groupId': 13
+                    },
+                    {
+                        'groupName': '初中组',
+                        'groupId': 14
+                    },
+                    {
+                        'groupName': '高中组',
+                        'groupId': 15
+                    }
+                ]
+            }
+        ],
+        'type': 0
+    },
+    'success': true
+}
+ */
