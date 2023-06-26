c832893f8043d9526e05c2aa43581e4000eb945f
火线兔 <silverhawk@foxmail.com>
2023-06-26 16:48:07 +0800

    TuuzMac

--- extend/shds/Response/FindBaby.php
+++ extend/shds/Response/FindBaby.php
@@ -18,8 +18,10 @@ class FindBaby extends Resp
         $this->code = $this->data['code'];
         if ($this->code != 20000) {
             $this->is_success = false;
+            $this->error = $this->data['data'];
+            return;
         }
-        $this->error = $this->data["data"];
+
         $this->records = $this->data["data"]["records"];
     }
 
