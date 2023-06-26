f8179cec02dbcf8d5e78de7a217fd4b23ce0786e
火线兔 <silverhawk@foxmail.com>
2023-06-26 12:04:51 +0800

    TuuzMac

--- extend/shds/Response/AddBaby.php
+++ extend/shds/Response/AddBaby.php
@@ -4,21 +4,20 @@ namespace shds\Response;
 
 class AddBaby extends Resp
 {
+
+    protected $code = 0;
+
     public function __construct($json)
     {
         parent::__construct($json);
-        if (!$this->isSuccess()){
-
+        if (!$this->isSuccess()) {
+            return $this;
         }
-        $this->error = $this->data;
-    }
-
-    /**
-     * @return string
-     */
-    public function getError(): string
-    {
-        return $this->error;
+        $this->code = $this->data['code'];
+        if ($this->code != 20000) {
+            $this->is_success = false;
+        }
+        $this->error = $this->data["data"];
     }
 
 }
