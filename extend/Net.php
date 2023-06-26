f27c57cb1d842af2e26ca7e2dde4d99a2c888828
火线兔 <silverhawk@foxmail.com>
2023-06-26 16:05:24 +0800

    TuuzMac

--- extend/Net.php
+++ extend/Net.php
@@ -93,11 +93,12 @@ class Net
         foreach ($merged_headers as $key => $value) {
             $headers[] = $key . ':' . $value;
         }
+        $file = ["file" => curl_file_create($fileData)];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $upload_url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fileData);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $file);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         $response = curl_exec($ch);
         curl_close($ch);
