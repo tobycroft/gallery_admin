841f85bd5f386296b399d41ef193e911ccf0f63d
火线兔 <silverhawk@foxmail.com>
2023-06-26 09:51:29 +0800

    TuuzMac

--- extend/shds/Login.php
+++ extend/shds/Login.php
@@ -6,13 +6,20 @@ class Login
 {
 
 
-    public function login($username, $password)
+    public function login($username, $password): string
     {
 
-        \Net::PostJson(config('shds_remote_url') . "/megagame/login/user/login", [], [
+        $json = \Net::PostJson(config('shds_remote_url') . "/megagame/login/user/login", [], [
             'code' => $username,
             'password' => $password,
             'sysflg' => ''
         ]);
+        $decode = json_decode($json, 1);
+        $success = (bool)$decode['success'];
+        $data = $decode['data'];
+        if ($success && !empty($data)) {
+            return $data["token"];
+        }
+        return "";
     }
 }
