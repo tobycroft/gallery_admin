42bebac130f478d96b9b40c11e8d36277268aa56
火线兔 <silverhawk@foxmail.com>
2023-06-26 17:05:10 +0800

    TuuzMac

--- extend/shds/Response/GetActivityList.php
+++ extend/shds/Response/GetActivityList.php
@@ -5,5 +5,36 @@ namespace shds\Response;
 class GetActivityList extends Resp
 {
 
-    
+    protected int $code = 0;
+
+    protected array $records = [];
+    protected array $map = [];
+
+    public function __construct($json)
+    {
+        parent::__construct($json);
+        if (!$this->isSuccess()) {
+            return $this;
+        }
+        $this->code = $this->data['code'];
+        if ($this->code != 20000) {
+            $this->is_success = false;
+            $this->error = $this->data['data'];
+            return;
+        }
+
+        $this->records = $this->data['data']['records'];
+        foreach ($this->records as $record) {
+            $map[$record["name"]] = $record["activityId"];
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
 }
