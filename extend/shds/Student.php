ea5f9d6723706e2c7d9e2a20dc65f30a32068cb3
火线兔 <silverhawk@foxmail.com>
2023-06-26 15:09:30 +0800

    TuuzMac

--- extend/shds/Student.php
+++ extend/shds/Student.php
@@ -32,7 +32,7 @@ class Student extends Login
         $fileData = file_get_contents("https://static.familyeducation.org.cn/gallery/20230612/9de27752e3aff3e7257989f4c7eda315.jpg");
         echo $this->token;
         $ret = \Net::PostBinary($fileData, config('shds_remote_url') . $path, ["Token" => $this->token]);
-        var_dump($ret);
+//        var_dump($ret);
 
 //        $resp = new Resp($ret);
 //        return $resp;
