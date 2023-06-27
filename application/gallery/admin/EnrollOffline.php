960920f066679d60c6131194be0eea73379cfb59
火线兔 <silverhawk@foxmail.com>
2023-06-27 16:34:55 +0800

    TuuzMac

--- application/gallery/admin/EnrollOffline.php
+++ application/gallery/admin/EnrollOffline.php
@@ -85,7 +85,7 @@ class EnrollOffline extends Admin
             ->addTopButton('export')
             ->setPageTitle('列表')
 //            ->addOrder('id,callsign,year,class')
-            ->setSearch(['name' => '学生姓名', 'phone' => '手机号', 'school_name' => '绑定单位', 'school_name_show' => '学校', 'is_send' => '已寄出']) // 设置搜索参数
+            ->setSearch(['name' => '学生姓名', 'phone' => '手机号', 'school_name' => '绑定单位', 'is_send' => '已寄出']) // 设置搜索参数
 
             ->addColumn('id', '问题ID')
 //            ->addColumn('source', '数据来源', 'number')
