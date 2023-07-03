196cc7843f3febec09fa1f4603b586fb166e5d44
火线兔 <silverhawk@foxmail.com>
2023-07-03 17:39:03 +0800

    TuuzMac

--- application/gallery/admin/EnrollUpload.php
+++ application/gallery/admin/EnrollUpload.php
@@ -32,7 +32,7 @@ class EnrollUpload extends Admin
         $order = $this->getOrder("id desc");
         $map = $this->getMap();
         // 读取用户数据
-        $data_list = EnrollUploadModel::where($map)->order($order)->paginate();
+        $data_list = EnrollUploadModel::alias("a")->where($map)->order($order)->paginate();
         $page = $data_list->render();
         $todaytime = date('Y-m-d H:i:s', strtotime(date("Y-m-d"), time()));
 
