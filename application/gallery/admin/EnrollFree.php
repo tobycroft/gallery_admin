343a370d25e2380599f4086be248f1758c587d94
火线兔 <silverhawk@foxmail.com>
2023-07-19 18:02:00 +0800

    TuuzMac

--- application/gallery/admin/EnrollFree.php
+++ application/gallery/admin/EnrollFree.php
@@ -52,7 +52,7 @@ class EnrollFree extends Admin
         $school = EnrollModel::column("id,name");
 
         return ZBuilder::make('table')
-            ->setPageTips("总数量：" . $num2 . "    今日数量：" . $num1 . "备注数量：", $num3, 'danger')
+            ->setPageTips('总数量：' . $num2 . '    今日数量：' . $num1 . '备注数量：' . $num3, 'danger')
 //            ->setPageTips("总数量：" . $num2, 'danger')
 //            ->setSearchArea([['select', 'school_id', '学校id', "", "", $school], ['text', 'year', '入学年份'], ['text', 'grade', '年级'], ['text', 'class', '班级'],])
             ->setSearchArea([
